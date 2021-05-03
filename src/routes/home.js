ed3d529ee0d1c6059ab37e0592b8cd0e2b1ada2c
lealclarissa <lealclarissa2@gmail.com>
2021-05-02 23:07:03 -0300

    update home

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,7 +5,7 @@ router.get("/", (req, res) => {
   res.status(200).send({
     title: "Meu crespo, minhas regras!",
     version: "1.0.0",
-    label: "Hackgrrrl 2021",
+    label: "Back-end - Hackgrrrl 2021",
     owner: "github.com/lealclarissa"
   });
 });
